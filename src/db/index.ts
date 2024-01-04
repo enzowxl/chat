ed36e04f62d400e8e01979395adf323c793db597
enzowxl <whayyzs13@gmail.com>
2024-01-04 13:10:03 -0300

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,13 +5,7 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6yU0ccgnUqOh7-8bJ0LXRc0VHTxwChns",
-  authDomain: "chatunify-47457.firebaseapp.com",
-  projectId: "chatunify-47457",
-  storageBucket: "chatunify-47457.appspot.com",
-  messagingSenderId: "570940649333",
-  appId: "1:570940649333:web:29ac3ea8f5eb1ab2afed3c",
-  measurementId: "G-Z241SQKLNN",
+
 };
 
 if (!getApps.length) {
